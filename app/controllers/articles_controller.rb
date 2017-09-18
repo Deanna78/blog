c7bc8014b374d103df166b7ce2bd516efbac4017
Deanna Garriga <deanna@dnarix.com>
2017-09-18 16:13:28 +0000

    Error messages added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,27 @@
 class ArticlesController < ApplicationController
   
-  def new
-  end
-  
-  def create
-    @article = Article.new(article_params)
-    
-    @article.save
-    redirect_to @article
+  def index
+    @articles = Article.all
   end
   
   def show
     @article = Article.find(params[:id])
   end
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+  
   
   
   
